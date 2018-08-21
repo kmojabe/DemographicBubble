aac77e4acdd334bd35300fca65bc63d760f57057
Kavian <kavianmojabe@gmail.com>
2018-08-20 20:18:39 -0700

    Today has been rough

--- frontend/world.jsx
+++ frontend/world.jsx
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { parse_data } from './parse';
+// document.addEventListener('DOMContentLoaded', () => {
+//
+//   const root = document.getElementById('root');
+// });
+import * as d3 from "d3";
 
-  const root = document.getElementById('root');
-});
+d3.select("#svg-container")
+    .append("circle")
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .attr("r", 20)
+        .attr("fill", "grey");
+
+console.log("still here");
+d3.selectAll('h1').style('color','red');
+
+d3.csv("population_data/population.csv").get(function(error,data){
+  console.log(data);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,19 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
   devtool: 'source-map'
 };
